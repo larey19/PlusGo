8e577629a5f70ad363da657562122f3a831c1080
Noel <noelenriqueascanio@gmail.com>
2026-02-18 15:53:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,3 @@ app.config.from_object(config)
 mysql = MySQL(app) 
 app.mysql=mysql
 routes(app)
-app.secret_key = os.getenv("SECRET_KEY")
-
-
-# Arrancar en la ruta ("http://127.0.0.1:6500")
-app.run(debug=True, port=6500, host="0.0.0.0")
