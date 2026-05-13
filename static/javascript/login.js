69b7cc5b41b6b659c39d9e441a5a680505d05a03
Noel <noelenriqueascanio@gmail.com>
2026-05-13 10:57:06 -0500

    Update login.js

--- static/javascript/login.js
+++ static/javascript/login.js
@@ -1,3 +1,6 @@
+AOS.init();
+// ========== ANIMACION CARGA DE PAGINA
+
 window.addEventListener("load", () => {
   NProgress.start();
   NProgress.done();
@@ -9,3 +12,27 @@ document.addEventListener("submit", () => {
     NProgress.inc();
   }, 600);
 });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/static/pwa/sw.js");
+}
+// ============= OJO PARA INPUT PASSWORD
+function password() {
+  const input = document.querySelectorAll("#password");
+  const icon = document.querySelectorAll("#eyeIcon");
+  input.forEach((psw) => {
+    if (psw.type === "password") {
+      psw.type = "text";
+      icon.forEach((icn) => {
+        icn.classList.remove("bi-eye");
+        icn.classList.add("bi-eye-slash");
+      });
+    } else {
+      psw.type = "password";
+      icon.forEach((icn) => {
+        icn.classList.remove("bi-eye-slash");
+        icn.classList.add("bi-eye");
+      });
+    }
+  });
+}
