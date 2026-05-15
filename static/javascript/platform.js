32c1a04f5a00a3ed232b46ea74bf6f3719af6cb6
Noel <noelenriqueascanio@gmail.com>
2026-05-15 15:06:54 -0500

    Create platform.js

---
+++ static/javascript/platform.js
@@ -0,0 +1,72 @@
+AOS.init();
+
+document.querySelectorAll(".dataPlatform").forEach((pla) => {
+  pla.addEventListener("click", function (platform) {
+    const pla_id = this.getAttribute("data-pla_id");
+
+    document.querySelector(".planame").value = this.dataset.pla_name;
+    document.querySelector(".plaprofiles").value = this.dataset.pla_profiles;
+    document.querySelector(".plamessage").value = this.dataset.pla_message;
+
+    document
+      .getElementById("validateFormCrt")
+      .addEventListener("click", function (clv) {
+        clv.preventDefault();
+        form = this.closest("#form_crt");
+        form.action = `/platform`;
+        if (form && form.checkValidity()) {
+          confirmCreate();
+        } else {
+          form.reportValidity();
+        }
+      });
+    document.querySelectorAll(".validateFormUpd").forEach((button) => {
+      button.addEventListener("click", function (clv) {
+        clv.preventDefault();
+        form = this.closest("#form_upd");
+        form.action = `/platform/${pla_id}`;
+        if (form && form.checkValidity()) {
+          confirmUpdate();
+        } else {
+          form.reportValidity();
+        }
+      });
+    });
+  });
+});
+
+function confirmUpdate(pla_id) {
+  Swal.fire({
+    title: "¿Actualizar Plataforma?",
+    icon: "info",
+    showCancelButton: true,
+    confirmButtonColor: "rgba(4,17,43,0.92)",
+    confirmButtonText: "Sí, guardar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.getElementById("form_upd").submit();
+    }
+  });
+}
+function confirmCreate() {
+  Swal.fire({
+    title: "¿Registar Plataforma?",
+    icon: "info",
+    showCancelButton: true,
+    confirmButtonColor: "rgba(4,17,43,0.92)",
+    confirmButtonText: "Sí, guardar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.getElementById("form_crt").submit();
+    }
+  });
+}
+$(document).ready(function () {
+  $("#table").DataTable({
+    language: {
+      url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json",
+    },
+  });
+});
